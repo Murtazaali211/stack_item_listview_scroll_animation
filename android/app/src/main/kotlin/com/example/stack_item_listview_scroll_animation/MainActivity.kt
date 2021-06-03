7675ea6d52123a98c0c4fa38820ca7207aba91b8
Murtazaali211 <59016029+Murtazaali211@users.noreply.github.com>
2021-06-03 11:40:59 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/stack_item_listview_scroll_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stack_item_listview_scroll_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,68 @@
+const FOOD_DATA = [
+  {
+    "name": "Burger",
+    "brand": "Hawkers",
+    "price": 2.99,
+    "image": "1.jpg"
+  },
+  {
+    "name": "Cheese Dip",
+    "brand": "Hawkers",
+    "price": 3.99,
+    "image": "2.jpg"
+  },
+  {
+    "name": "Chicken Burger",
+    "brand": "Mcdonald",
+    "price": 4.99,
+    "image": "3.jpg"
+  },
+  {
+    "name": "Fries",
+    "brand": "Mcdonald",
+    "price": 2.99,
+    "image": "4.jpg"
+  },
+  {
+    "name": "Beef Burger",
+    "brand": "Dominos",
+    "price": 12.99,
+    "image": "5.jpg"
+  },
+  {
+    "name": "Sandwich",
+    "brand": "Hawkers",
+    "price": 2.99,
+    "image": "1.jpg"
+  },
+  {
+    "name": "Sandwich",
+    "brand": "Hawkers",
+    "price": 2.99,
+    "image": "1.jpg"
+  },
+  {
+    "name": "Fries",
+    "brand": "Mcdonald",
+    "price": 2.99,
+    "image": "4.jpg"
+  },
+  {
+    "name": "Sandwich",
+    "brand": "Hawkers",
+    "price": 2.99,
+    "image": "1.jpg"
+  },
+  {
+    "name": "Fries",
+    "brand": "Mcdonald",
+    "price": 2.99,
+    "image": "4.jpg"
+  },
+  {
+    "name": "Sandwich",
+    "brand": "Hawkers",
+    "price": 2.99,
+    "image": "1.jpg"
+  }
+];
